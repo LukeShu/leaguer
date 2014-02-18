c9520c2c65e4997c7fea8b067f7f2806aab44b85
AndrewMurrell <amurrel@purdue.edu>
2014-02-18 18:16:28 -0500

    Added the generate shell command. Please do not commit the generated files.

---
+++ generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bundle exec rails generate scaffold server
+bundle exec rails generate scaffold tournament
+bundle exec rails generate scaffold match
+bundle exec rails generate scaffold team
+bundle exec rails generate scaffold user
+#bundle exec rails generate scaffold 
