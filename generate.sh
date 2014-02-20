1b0022eca5cfbab8bfee72030d45443bf9d44af7
DavisLWebb <davislwebb@ymail.com>
2014-02-20 17:13:51 -0500

    fix datatype names in generate.sh

--- generate.sh
+++ generate.sh
@@ -10,13 +10,13 @@
 set -x
 bundle exec rails generate scaffold server
 bundle exec rails generate scaffold tournament
-bundle exec rails generate scaffold match tournament:reference
+bundle exec rails generate scaffold match tournament:references
 bundle exec rails generate scaffold team 
-bundle exec rails generate scaffold user name:text pw_hash:varchar groups:int 
-bundle exec rails generate model user_team_pair user:reference team:reference
-bundle exec rails generate model team_match_pair team:reference match:reference
-bundle exec rails generate scaffold alert author:reference message:text
-bundle exec rails generate scaffold pm author:reference recipient:reference message:text
+bundle exec rails generate scaffold user name:text pw_hash:text groups:integer 
+bundle exec rails generate model user_team_pair user:references team:references
+bundle exec rails generate model team_match_pair team:references match:references
+bundle exec rails generate scaffold alert author:references message:text
+bundle exec rails generate scaffold pm author:references recipient:references message:text
 bundle exec rails generate model server_settings
 bundle exec rails generate controller search
 bundle exec rails generate controller main
