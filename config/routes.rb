9d64e8898f767b42ce231b34abe4333007d7d0df
Tomer Kimia <tkimia@purdue.edu>
2014-03-01 21:31:39 -0500

    Added bootstrap and games to routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Leaguer::Application.routes.draw do
 
   resources :servers
 
+  resources :games
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
