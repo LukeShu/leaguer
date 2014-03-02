2426a2b1e5b6811f47f0a05dd66a001fdd117450
DavisLWebb <davislwebb@ymail.com>
2014-03-02 18:04:36 -0500

    fixed an issue in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      #redirect_to @user
     else
       render 'new'
     end
