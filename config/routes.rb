d28c9b6f33ae4a95d620d46caa3799292b39480b
Tomer Kimia <tkimia@purdue.edu>
2014-03-02 11:11:04 -0500

    added preliminary homepage

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Leaguer::Application.routes.draw do
 
   resources :games
 
-  root "tournaments#index"
+  root to: 'static#homepage'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
