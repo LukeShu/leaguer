faa08e4f2dbc6fb37619df7375638c1a7749ef80
Luke Shumaker <LukeShu@sbcglobal.net>
2014-03-01 23:18:45 -0500

    mv docs doc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304015644) do
+ActiveRecord::Schema.define(version: 20140304015944) do
 
   create_table "alerts", force: true do |t|
     t.integer  "author_id"
