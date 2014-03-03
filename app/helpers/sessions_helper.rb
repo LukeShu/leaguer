d6cd8be7b4b5453e93faf11858c3e888f23a8ed9
DavisLWebb <davislwebb@ymail.com>
2014-03-03 12:22:57 -0500

    I HAVE THE WEBSITE RENDER THE USER'S USER NAME

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,9 +14,13 @@ module SessionsHelper
 
 #method creating for self.current_user
 	def current_user=(user)
-		remember_token = User.hash(cookies[:remember_token])
-		@current_user ||= User.find_by(remember_token: remember_token)
+		@current_user = user
 	end
+
+  def current_user
+    remember_token = User.hash(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
 
 	# checks if someone is currently signed in
 	def signed_in?
