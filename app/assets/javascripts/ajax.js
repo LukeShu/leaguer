def21c442c6e4b52b50f852cedd3e8bcaf436f9d
AndrewMurrell <amurrel@purdue.edu>
2014-03-03 13:17:02 -0500

    Changed the views for tournament a little more.

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -3,9 +3,10 @@ function populate() {
 	//make a form element
 	var e = document.getElementById("tournament_id");
 	var gameType = e.options[e.selectedIndex].text;
-	if (gameType != "") {
+	if (gameType != "Select a Game Type") {
+		alert(gameType + " was Selected!");
 		//populate optionArray
-		
+		//select * from tournament_settings where gametype = GameType
 		for(var option in optionArray){
 			//identify the number of 
 			;
