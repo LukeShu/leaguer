114b7d13be2dbba1ce3bf3c344396c587e464e50
Luke Shumaker <LukeShu@sbcglobal.net>
2014-03-03 21:48:32 -0500

    fiddle with generate.sh

--- generate.sh
+++ generate.sh
@@ -10,7 +10,7 @@
 # To Clear the Generated Files:
 #   git clean -df
 #
-NOTEST='--skip-test-unit'
+#NOTEST='--skip-test-unit'
 
 set -x
 bundle exec rails generate scaffold server --force $NOTEST
@@ -20,18 +20,17 @@ bundle exec rails generate scaffold team $NOTEST
 bundle exec rails generate scaffold alert author:references message:text $NOTEST
 bundle exec rails generate scaffold pm author:references recipient:references message:text $NOTEST
 bundle exec rails generate scaffold game name:text players_per_team:integer teams_per_match:integer set_rounds:integer randomized_teams:integer --force $NOTEST
+bundle exec rails generate scaffold user name:string email:string user_name:string $NOTEST
 
 bundle exec rails generate model game_attribute game:references key:text type:integer $NOTEST
 bundle exec rails generate model server_settings $NOTEST
-bundle exec rails generate model user name:string email:string user_name:string $NOTEST
 bundle exec rails generate model user_team_pair user:references team:references $NOTEST
 bundle exec rails generate model team_match_pair team:references match:references $NOTEST
 
 bundle exec rails generate controller search $NOTEST
 bundle exec rails generate controller main $NOTEST
 bundle exec rails generate controller static $NOTEST
-bundle exec rails generate controller users $NOTEST
-bundle exec rails generate controller Sessions
+bundle exec rails generate controller sessions
 
 #added some stuff to the database
 
