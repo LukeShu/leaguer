661dbaf902d770dfbefa9ba1d9e4c28fc7abf891
Tomer Kimia <tkimia@purdue.edu>
2014-03-03 21:49:38 -0500

    small things

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,25 @@
 class SessionsController < ApplicationController
 
 	def new
+		if @user.nil?
+			@user = User.new
+		end
 	end
 	
 	# find the user and create a new session
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-			sign_in user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+			sign_in @user
 		  redirect_to root_path
     else
-			render 'new'
-    end
+		  redirect_to signin_path
+	end
   end
 
 	def destroy
-    sign_out
-    redirect_to root_path
+    	sign_out
+    	redirect_to root_path
 	end
 
 end
