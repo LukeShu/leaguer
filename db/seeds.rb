7243bc441795b70249d99bd43a4a0a88c042f91f
Tomer Kimia <tkimia@purdue.edu>
2014-03-07 17:29:15 -0500

    notices in center, league of legends seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-Game.create(name: "League of Legends",min_players_per_team: 5,  max_players_per_team: 5, min_teams_per_match: 5, max_teams_per_match: 5, set_rounds: nil, randomized_teams: false)
+Game.create(name: "League of Legends",min_players_per_team: 5,  max_players_per_team: 5, min_teams_per_match: 2, max_teams_per_match: 2, set_rounds: nil, randomized_teams: false)
 
 
