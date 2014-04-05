d421e2ce782c00fa77f6aa2f18d54eb429000c9f
Luke Shumaker <shumakl@purdue.edu>
2014-04-04 21:11:10 -0400

    fix the spelling of "description" in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ Game.create(name: "Chess", min_players_per_team: 1,  max_players_per_team: 1, mi
 Game.create(name: "Hearthstone", min_players_per_team: 1, max_players_per_team: 1, min_teams_per_match: 2, max_teams_per_match: 2, set_rounds: 1, randomized_teams: false)
 
 
-GameSetting.create(game_id: 0, name: "Map", default: "Summoners Rift,Twisted Treeline,Crystal Scar,Haunted Abyss", discription: "Select a map to play on.", type_opt: "Select", display_order: 1)
-GameSetting.create(game_id: 0, name: "Pick Type", default: "Blind Pick,Draft", discription: "Select a pick type.", type_opt: "Select", display_order: 2)
+GameSetting.create(game_id: 0, name: "Map", default: "Summoners Rift,Twisted Treeline,Crystal Scar,Haunted Abyss", description: "Select a map to play on.", type_opt: "Select", display_order: 1)
+GameSetting.create(game_id: 0, name: "Pick Type", default: "Blind Pick,Draft", description: "Select a pick type.", type_opt: "Select", display_order: 2)
 
-#Game_setting.create(game_id: , type: , name: , default: , discription: , type_opt: , display_order: , created_at: , updated_at: )
+#Game_setting.create(game_id: , type: , name: , default: , description: , type_opt: , display_order: , created_at: , updated_at: )
 
 #User.create(id: 1, name: "Andrew", email: "amurrel@example.com", user_name: "ImFromNASA", created_at: "2014-03-07 22:48:53", updated_at: "2014-03-07 22:48:53", password_digest: "$2a$10$W0FwPRDzdQp8arolCHGt5ezXqkOiTLNsXI5GKGtu9qr3...", remember_token: "1583722b02663e024533296171cad79f29037ebc", groups: 0)
