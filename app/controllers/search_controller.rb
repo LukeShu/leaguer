bb53f8ac2ab52301b32fe65ddc10a32f6bc730e8
tkimia <tkimia@purdue.edu>
2014-04-22 17:42:57 -0400

    prelim search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,13 @@
 class SearchController < ApplicationController
+
+	def go
+		@query = params[:query]
+
+		if (@query.nil?) return;
+
+		@tournaments = Tournament.where("name LIKE '#{@query}'")
+		@players = User.where("name LIKE '#{@query}")
+
+	end
+
 end
