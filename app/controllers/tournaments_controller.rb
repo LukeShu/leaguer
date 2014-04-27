62219ac4e3af36cc15c9ad61701550bee9a8424c
Luke Shumaker <shumakl@purdue.edu>
2014-04-26 20:16:33 -0400

    run ./generate.sh

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -69,6 +69,6 @@ class TournamentsController < ApplicationController
 
 	# Never trust parameters from the scary internet, only allow the white list through.
 	def tournament_params
-		params.require(:tournament).permit(:game_id, :status, :name, :min_players_per_team, :max_players_per_team, :min_teams_per_match, :max_teams_per_match, :set_rounds, :randomized_teams, :sampling_method)
+		params.require(:tournament).permit(:game_id, :status, :name, :min_players_per_team, :max_players_per_team, :min_teams_per_match, :max_teams_per_match, :set_rounds, :randomized_teams, :sampling_method, :scoring_method)
 	end
 end
--- app/views/games/show.json.jbuilder
+++ app/views/games/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @game, :id, :parent_id, :name, :min_players_per_team, :max_players_per_team, :min_teams_per_match, :max_teams_per_match, :set_rounds, :randomized_teams, :sampling_method, :created_at, :updated_at
+json.extract! @game, :id, :parent_id, :name, :min_players_per_team, :max_players_per_team, :min_teams_per_match, :max_teams_per_match, :set_rounds, :randomized_teams, :sampling_method, :scoring_method, :created_at, :updated_at
