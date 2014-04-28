55f4c94d089ce622bd4fe0b4c915e8e4cb4122aa
AndrewMurrell <amurrel@purdue.edu>
2014-04-28 19:32:32 -0400

    changes to peer review and manual sampling

--- lib/sampling/manual.rb
+++ lib/sampling/manual.rb
@@ -40,7 +40,7 @@ module Sampling
 			erb_filename = File.join(__FILE__.sub(/\.rb$/, '.html.erb'))
 			erb = ERB.new(File.read(erb_filename))
 			erb.filename = erb_filename
-			return erb.result.html_safe
+			return erb.result(binding).html_safe
 		end
 
 		def handle_user_interaction(user, sampling_params)
--- lib/sampling/peer_review.rb
+++ lib/sampling/peer_review.rb
@@ -39,7 +39,7 @@ module Sampling
 			erb_filename = File.join(__FILE__.sub(/\.rb$/, '.html.erb'))
 			erb = ERB.new(File.read(erb_filename))
 			erb.filename = erb_filename
-			return erb.result.html_safe
+			return erb.result(binding).html_safe
 		end
 
 		def handle_user_interaction(reviewing_user, params)
