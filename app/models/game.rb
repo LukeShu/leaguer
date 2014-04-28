b8ce4ab9ad7e6359080e894958a3181cbb3895d2
Luke Shumaker <shumakl@purdue.edu>
2014-04-27 20:24:36 -0400

    touch up tournament creation

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ActiveRecord::Base
 	belongs_to :parent, class_name: "Game"
 	has_many :children, class_name: "Game"
-	has_many :settings, class_name: "GameSetting"
+	has_many :game_settings
+
+	alias_attribute :settings, :game_settings
 end
